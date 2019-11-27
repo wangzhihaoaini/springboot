6315961a28e3d3c9beeb5cb46dd4600e454cf0c8
wangzhihaoaini <wzh825250174@gmail.com>
2019-11-27 22:58:52 +0800

    修改日志存放位置和级别

--- src/main/java/com/wangzhihao/springboot/SpringbootApplication.java
+++ src/main/java/com/wangzhihao/springboot/SpringbootApplication.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @MapperScan(value = "com.wangzhihao.springboot.mapper")  //扫描mapper接口
+//@EnableScheduling  //开启基于注解的定时任务
 @EnableCaching  //开启基于注解的缓存
 @SpringBootApplication
-public class DemoApplication {
+public class SpringbootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 
 }
