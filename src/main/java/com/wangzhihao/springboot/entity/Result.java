e85392fdb77733b832fdaaf900036d3f711436a1
wangzhihaoaini <wzh825250174@gmail.com>
2019-11-29 14:56:04 +0800

    结果实体类

---
+++ src/main/java/com/wangzhihao/springboot/entity/Result.java
@@ -0,0 +1,38 @@
+package com.wangzhihao.springboot.entity;
+
+/**
+ * @description：操作结果集
+ * @author：wangzhihao
+ * @date：2019/11/29 14:53
+ */
+public class Result{
+    public static final int SUCCESS = 1;
+    public static final int FAILURE = -1;
+    private boolean success;
+    private String msg = "";
+    private Object obj = null;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+}
